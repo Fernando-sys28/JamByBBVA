7148f83970f427cb5ebcb1c7626fd128c728d388
ctochimani <122694117+ctochimani@users.noreply.github.com>
2024-07-12 13:49:24 -0600

    eso es todo?

--- JamBBVA/ContentView.swift
+++ JamBBVA/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("paps te amo")
+            Text("eso es todo?")
         }
         .padding()
     }
