a63d059cd2c98f415a40b7a369b18268751140d5
ctochimani <122694117+ctochimani@users.noreply.github.com>
2024-07-12 14:13:39 -0600

    hubi

--- JamBBVA/ContentView.swift
+++ JamBBVA/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Paps te amo")
             Text("eso es todo?")
-            Text("a pmienta ya le jalo")
+            Text("a pmienta ya le jalo")hubvuyhv bhkujb
         }
         .padding()
     }
