eea2621d544c3c899970148e645d3c8f84324592
Fernando Rivera Castillo <116294941+Fernando-sys28@users.noreply.github.com>
2024-07-12 15:00:43 -0600

    te vistes feo

---
+++ JamBBVA/ceci.swift
@@ -0,0 +1,18 @@
+//
+//  ceci.swift
+//  JamBBVA
+//
+//  Created by Fernando Rivera Castillo on 12/07/24.
+//
+
+import SwiftUI
+
+struct ceci: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ceci()
+}
