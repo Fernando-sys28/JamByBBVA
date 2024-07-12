d10bc541cc3d9236b392b3eee6b5b00bff922330
Fernando Rivera Castillo <116294941+Fernando-sys28@users.noreply.github.com>
2024-07-12 15:01:17 -0600

    ceci

--- JamBBVA/ceci.swift
+++ JamBBVA/ceci.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct ceci: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("te vistes feo")
+        Image("ceci")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 150)
     }
 }
 
