ce088427fa3d9e4ca9eb4c3c74a899cee54b9a96
Fernando Rivera Castillo <116294941+Fernando-sys28@users.noreply.github.com>
2024-07-12 13:20:19 -0600

    Initial Commit

---
+++ JamBBVA/JamBBVAApp.swift
@@ -0,0 +1,17 @@
+//
+//  JamBBVAApp.swift
+//  JamBBVA
+//
+//  Created by Fernando Rivera Castillo on 12/07/24.
+//
+
+import SwiftUI
+
+@main
+struct JamBBVAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
