83528246157d32f006801d9d2ba05f78634ce29b
Fernando Rivera Castillo <116294941+Fernando-sys28@users.noreply.github.com>
2024-07-12 13:31:33 -0600

    hola paps

--- JamBBVA/ContentView.swift
+++ JamBBVA/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, fer te tqm :)")
+            Text("paps te amo")
         }
         .padding()
     }
